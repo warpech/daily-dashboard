5587f9110ca004d4921b15ec850e6f61c0b5a2c9
Marcin Warpechowski <warpech@gmail.com>
2013-11-29 14:31:33 +0100

    render as a pivot table

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ $(function () {
     var url = '';
     switch (value) {
       case 'x-html':
-//        url = 'https://github.com/PuppetJs/x-html';
         url = 'http://puppetjs.github.io/x-html/examples/index.html';
         break;
 
@@ -17,48 +16,42 @@ $(function () {
   }
 
   function statusRenderer(instance, TD, row, col, prop, value, cellProperties) {
-    switch (value) {
-      case 'green':
-        TD.style.backgroundColor = 'lime';
-        Handsontable.Dom.fastInnerHTML(TD, 'OK');
-        break;
-
-      case 'yellow':
-        TD.style.backgroundColor = 'yellow';
-        Handsontable.Dom.fastInnerHTML(TD, 'Works with workaround');
-        break;
-
-      case 'red':
-        TD.style.backgroundColor = 'red';
-        Handsontable.Dom.fastInnerHTML(TD, 'Fail');
-        break;
+    if (!value) {
+      value = {
+        status: 'white',
+        comment: 'N/A'
+      };
     }
-  }
 
-  function recommendationRenderer(instance, TD, row, col, prop, value, cellProperties) {
-    var browserRecommendation = browserGecommendation(instance.getData(), instance.getDataAtRow(row).browser);
-    switch (browserRecommendation) {
-      case 'red':
-        TD.style.backgroundColor = 'PeachPuff';
+    switch (value.status) {
+      case 'green':
+        TD.style.backgroundColor = 'lightgreen';
+        Handsontable.Dom.fastInnerHTML(TD, 'OK ' + value.comment);
         break;
 
       case 'yellow':
         TD.style.backgroundColor = '#FFFF99';
+        Handsontable.Dom.fastInnerHTML(TD, 'WORKS WITH WORKAROUND ' + value.comment);
         break;
 
-      case 'green':
-        TD.style.backgroundColor = 'lightgreen';
+      case 'red':
+        TD.style.backgroundColor = 'tomato';
+        Handsontable.Dom.fastInnerHTML(TD, 'FAIL ' + value.comment);
+        break;
+
+      case 'white':
+        TD.style.backgroundColor = 'white';
+        Handsontable.Dom.fastInnerHTML(TD, '? ' + value.comment);
         break;
     }
-    Handsontable.cellLookup.renderer.text.apply(this, arguments);
   }
 
-  function browserGecommendation(data, browser) {
+  function browserRecommendation(data, browser) {
     var scores = {
       red: 0,
       yellow: 0,
       green: 0
-    }
+    };
     for (var i = 0, ilen = data.length; i < ilen; i++) {
       if (data[i].browser === browser) {
         scores[data[i].status]++;
@@ -81,7 +74,7 @@ $(function () {
     for (var i = 0, ilen = data.length; i < ilen; i++) {
       if (!checked[data[i].browser]) {
         checked[data[i].browser] = true;
-        if (browserGecommendation(data, data[i].browser) === 'green') {
+        if (browserRecommendation(data, data[i].browser) === 'green') {
           out.push(data[i].browser);
         }
       }
@@ -90,7 +83,6 @@ $(function () {
   }
 
   function htmlRenderer(instance, TD, row, col, prop, value, cellProperties) {
-    recommendationRenderer.apply(this, arguments);
     Handsontable.Dom.fastInnerHTML(TD, value);
   }
 
@@ -102,55 +94,84 @@ $(function () {
     return d + ' ' + m + ',  ' + y;
   }
 
-  $("#hot").handsontable({
-    readOnly: true,
-    data: [],
-    renderer: recommendationRenderer,
-    columns: [
-      {
-        title: "Project",
-        data: "project",
-        renderer: projectRenderer
-      },
-      {
-        title: "Project version",
-        data: "project_version"
-      },
-      {
-        title: "Browser",
-        data: "browser"
-      },
-      {
-        title: "Browser version",
-        data: "browser_version"
-      },
-      {
-        title: "Status",
-        data: "status",
-        renderer: statusRenderer
-      },
-      {
-        title: "Comment",
-        data: "comment",
-        width: 330,
-        renderer: htmlRenderer
-      }
-    ]
-  });
-
-  var lastIndex = 0;
-  var currentIndex = 0;
   var data;
+  var lastIndex;
+  var currentIndex;
+
   $.get("data/data.json", function (response) {
     data = response;
     lastIndex = data.length - 1;
     currentIndex = lastIndex;
-    renderIndex(currentIndex);
+
+    $("#hot").handsontable({
+      readOnly: true,
+      data: [],
+      renderer: statusRenderer,
+      columns: [
+        {
+          title: "Project",
+          data: "project",
+          renderer: projectRenderer
+        }
+      ]
+    });
+
+    renderIndex(lastIndex);
+
     $("#lastUpdate").text(dateToHumanDate(data[lastIndex].date));
   });
 
+  function createPivot(data, columns) {
+    var columnNames = [];
+    var pivot = [];
+    data.forEach(function (dataRow) {
+      if (columnNames.indexOf(dataRow.browser) === -1) {
+        columnNames.push(dataRow.browser);
+      }
+
+      var pivotRow = pivot.filter(function (pivotRow) {
+        return pivotRow.project === dataRow.project;
+      });
+      if (pivotRow.length) {
+        pivotRow = pivotRow[0];
+      }
+      else {
+        pivotRow = {};
+        pivotRow.project = dataRow.project;
+        pivot.push(pivotRow);
+      }
+
+      pivotRow[dataRow.browser] = {
+        status: dataRow.status,
+        comment: dataRow.comment
+      };
+    });
+
+    columnNames.forEach(function (columnName) {
+      columns.push({
+        title: columnName,
+        data: columnName
+      });
+    });
+
+    return pivot;
+  }
+
   function renderIndex(index) {
-    $("#hot").handsontable('loadData', data[index].statuses);
+    var columns = [
+      {
+        title: "Project",
+        data: "project",
+        renderer: projectRenderer
+      }
+    ];
+    var pivot = createPivot(data[index].statuses, columns);
+
+    $("#hot").handsontable('loadData', pivot);
+    $("#hot").handsontable('updateSettings', {
+      columns: columns
+    });
+
     $("#recommendation").html(data[index].recommendation || '');
     $("#currentDate").text(dateToHumanDate(data[index].date));
     $("#greenBrowsers").text(greenBrowsers(data[index].statuses).join(', ') || "none");
